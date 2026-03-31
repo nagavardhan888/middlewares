b4f42a158ab9ebf36a2a324e7bf1f8fd9af44c8c
naga vardhan <nagavardhanK@gmail.com>
2026-03-31 16:50:27 +0530

    learning middle wares

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const server = http.createServer((req,res)=>{
+  res.statusCode = 200 
+  res.end('bye')
+})
+server.listen(3000,()=>{
+    console.log("3000")
+})
---
+++ expresserror.js
@@ -0,0 +1,8 @@
+class expresserror extends error{
+    constructor(status,message){
+        super()
+        this.status = status
+        this.message = message
+    }
+}
+module.exports =expresserror
