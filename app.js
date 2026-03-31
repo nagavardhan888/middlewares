0d7017a496d13f388bc19a0389e80361887b63dd
nagavardhan888 <nagavardhan@gmail.com>
2026-03-31 16:59:58 +0530

    first change

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require('http')
 const server = http.createServer((req,res)=>{
   res.statusCode = 200 
   res.end('bye')
+  
 })
+
 server.listen(3000,()=>{
     console.log("3000")
 })
