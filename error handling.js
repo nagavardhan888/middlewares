c0fd170475222f5de961b59fbf9e9c279656a0d1
nagavardhan888 <nagavardhan@gmail.com>
2026-04-01 23:26:46 +0530

    first error handling

---
+++ error handling.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+// The presence of that fourth argument tells Express: "Hey, only run this function if something went wrong earlier in the chain."
+app.use((err,req,res,next)=>{
+    console.error(err)
+    res.status(500).send("something broke")
+})
+app.get("/profile",(req,res)=>{
+    const user = db.findById()
+    if(!user){
+        const error = new Error("user not found")
+        
+    }
+}) 
+
+app.listen(5000,()=>{
+    console.log("running at port 5000")
+})
