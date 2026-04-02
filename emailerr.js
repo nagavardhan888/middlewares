2eef8f94bdbbf67378506e08a54a457af9f66806
nagavardhan888 <nagavardhankaranam@gmail.com>
2026-04-03 00:02:17 +0530

    email error handling

---
+++ emailerr.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send("home page")
+})
+app.post("/email",(req,res,next)=>{
+    const email = req.body.email;
+    if(!email){
+        const err = new Error("email is required")
+        err.status= 400
+         return next(err)
+    }
+    res.send(`email ${email} received successfully`)
+})
+app.use((req,res,next)=>{
+    const err = new Error("route not found")
+    err.status = 401;
+    next(err)
+})
+app.use((err,req,res,next)=>{
+    const status = err.status || 500
+    res.status(status).json({
+        "oops ! our server tripped":err.message,
+        "status":status
+    })
+    console.log(`Error ${status} :  ${err.message}`)    
+    })
+app.listen(5000,()=>{
+    console.log("running at port 5000")
+})
