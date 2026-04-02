52234216630f74d694671bfdea1b2fd733c68426
nagavardhan888 <nagavardhankaranam@gmail.com>
2026-04-02 23:25:47 +0530

    async error handling

--- error.js
+++ error.js
@@ -3,6 +3,11 @@ const app = express()
 app.get("/",(req,res)=>{
     res.send("home page")
 })
+// async error handling
+app.get("/user",async(req,res,next)=>{
+    const user = await user.find()
+    res.send(user)
+})
 app.get("/errortest",(req,res,next)=>{
   const err = new Error("manual eroor")
   err.status=400
@@ -15,12 +20,13 @@ app.use((req,res,next)=>{
 })
 app.use((err,req,res,next)=>{
     const status = err.status || 500
-    console.log(`Error ${status} : {err.message}`)
+    
     res.status(status).json({
-        success:false,
-        message:err.message,
+        "oops ! our server tripped":err.message,
     })
+    console.log(`Error ${status} : {err.message}`)
 })
+
 app.listen(5000,()=>{
     console.log("running at port 5000")
 })
